5a544e5a1debaabbb870dfcbc76dc78cc11b020e
iskandar1023 <iiiiskandar14@gmail.com>
2016-08-22 14:11:08 +0700

    Remove "packages_changed" key at boot

--- app/src/main/java/projekt/substratum/services/BootCompletedDetector.java
+++ app/src/main/java/projekt/substratum/services/BootCompletedDetector.java
@@ -3,6 +3,8 @@ package projekt.substratum.services;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 
 /**
  * @author Nicholas Chum (nicholaschum)
@@ -12,6 +14,9 @@ public class BootCompletedDetector extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         if ("android.intent.action.BOOT_COMPLETED".equals(intent.getAction())) {
+            SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(context).edit();
+            editor.remove("packages_changed");
+            editor.apply();
             Intent pushIntent = new Intent(context, ThemeService.class);
             context.startService(pushIntent);
         }
